c8954a75584ddcf31d6d783c2203365db429ee66
HughesSeanKyle <khughessean@yahoo.com>
2021-02-22 08:23:50 +0200

    V132 - Solution: React.lazy() code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
+// Part 1 - No code splitting
 // import Page2 from './components/Page2';
 // import Page3 from './components/Page3';
-import AsyncComponent from './components/AsyncComponent';
+
+// Part 3 - Cleaner Code Splitting
+// import AsyncComponent from './components/AsyncComponent';
+
+// Part 4 - React.lazy
+const Page2Lazy = React.lazy(() => import('./components/Page2')); // 3
+const Page3Lazy = React.lazy(() => import('./components/Page3')); // 3
+
 
 
 class App extends Component {
@@ -38,21 +46,44 @@ class App extends Component {
 
   render() {
     // Part 3 - Cleaner Code Splitting
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   const AsyncPage2 = AsyncComponent(() => import("./components/Page2"));
+    //   return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    // } else {
+    //   const AsyncPage3 = AsyncComponent(() => import("./components/Page3"));
+    //   return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    // }
+    
+    // Part 4 - React.lazy
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
     } else if (this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(() => import("./components/Page2"));
-      return <AsyncPage2 onRouteChange={this.onRouteChange} />
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page2Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      ); 
     } else {
-      const AsyncPage3 = AsyncComponent(() => import("./components/Page3"));
-      return <AsyncPage3 onRouteChange={this.onRouteChange} />
-    } 
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page3Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      );
+    }
   };
 };
 
 export default App;
 
 /*
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+NB - Check ANDREA REPO for different steps
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
 // 1
 Shows old state onClick and renders new page
 onClick page2 => state : page1 => renders page2
@@ -70,4 +101,14 @@ This code splitting will be achieved by using dynamic imports. Only import compo
 // 2
 Where does state.component come from? 
 this is a state we create called component 
+
+// 3 
+Function that dynamically imports  component
+lazy component says - only load up this component, be lazy and only load up when we use it in the render function. 
+
+Lazy component must be rendered inside of a suspense component
+  Shows some fallback content such as a loading indicator
+    While we wait for component to load 
+  Always wrap lazy component in a suspense component (new feature of react)
+  Suspense component must have fallback attribute and must return a react component
 */
